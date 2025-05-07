9c393b0a4ee68c0e88732a871718e0f17b30afca
Bartek <bartekswiridow@gmail.com>
2025-05-07 14:26:23 +0200

    fdsafas

--- src/components/Contact/ContactPage.tsx
+++ src/components/Contact/ContactPage.tsx
@@ -93,28 +93,6 @@ const ContactPage: React.FC = () => {
 
   return (
     <>
-      {/* Dodajemy ukryty formularz HTML dla bota Netlify */}
-      <div hidden>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <input type="hidden" name="bot-field" />
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <input type="text" name="company" />
-          <select name="interest">
-            <option value="strony-internetowe">Strona internetowa</option>
-            <option value="automatyzacja">Automatyzacja</option>
-          </select>
-          <textarea name="message"></textarea>
-          <input type="checkbox" name="privacyPolicy" />
-        </form>
-      </div>
-
       {/* Hero Section */}
       <section className="relative pt-32 pb-20 md:pt-40 md:pb-28 overflow-hidden">
         {/* Tło sekcji z gradientem jak na stronie głównej */}
@@ -194,10 +172,12 @@ const ContactPage: React.FC = () => {
                 name="contact"
                 method="POST"
                 data-netlify="true"
+                netlify-honeypot="bot-field"
                 className="space-y-6"
                 onSubmit={handleSubmit}
               >
                 <input type="hidden" name="form-name" value="contact" />
+                <input type="hidden" name="bot-field" />
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <motion.div
                     initial={{ opacity: 0, y: 20 }}
